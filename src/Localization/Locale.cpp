fcec4017ffba8b12b9664127a7670e494413e6f5
Arsel Muginov <arsel.muginov@gmail.com>
2021-02-05 00:47:05 +0500

    Add hack to fill static locale maps only once

--- src/Localization/Locale.cpp
+++ src/Localization/Locale.cpp
@@ -7,16 +7,19 @@ std::unordered_map<std::string, std::filesystem::path> Locale::m_localePaths;
 
 Locale::Locale(std::string_view startupLocaleKey) : m_data{}
 {
-	for (const auto& entry : std::filesystem::directory_iterator(Paths::locales))
+	if (m_localeNames.empty())
 	{
-		std::string localeKey = entry.path().filename().string();
+		for (const auto& entry : std::filesystem::directory_iterator(Paths::locales))
+		{
+			std::string localeKey = entry.path().filename().string();
 
-		std::ifstream nameFile(entry.path() / c_nameFileName);
-		std::string localeName;
-		nameFile >> localeName;
+			std::ifstream nameFile(entry.path() / c_nameFileName);
+			std::string localeName;
+			nameFile >> localeName;
 
-		m_localeNames.insert({localeKey, localeName});
-		m_localePaths.insert({localeKey, entry.path() / c_dataFileName});
+			m_localeNames.insert({localeKey, localeName});
+			m_localePaths.insert({localeKey, entry.path() / c_dataFileName});
+		}
 	}
 
 	load(startupLocaleKey);
